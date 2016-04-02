54c2167e96276727065c142a1cb1160ec8569fb6
nxaxg <nxayancanxg@gmail.com>
2016-04-01 21:01:53 -0300

    Menu buttons action.
    Added JS and CSS to animate burguer menu, hidden categories and lang
    button.

--- site-puntoticket/js/script.js
+++ site-puntoticket/js/script.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
-	
+	//langmenu
+    $(".lang").hide();
+    $('#langbtn').click(function(){
+        $(this).toggleClass("lang-action");
+        $('.lang').slideToggle(500); 
+    });
+    
     //burguer-menu action
+    $(".navmenu").hide();   
     $('#menuicon').click(function(){
-		$(this).toggleClass('menu-action');
+        $('#menuicon').toggleClass("menu-action");
+        $(".navmenu").slideToggle(500);
 	});
+    
+    //cat-menu
+    $(".cat-menu").hide();
+    $("#catbtn").click(function(){
+        $("#cat-arrow").toggleClass("cat-action");
+       $(".cat-menu").slideToggle(500); 
+    });
     
     //slider action
     //http://unslider.com/
     $('#slider-main').unslider({
         autoplay: true,
         infinite: true,
-        delay: 8000,
+        delay: 5000,
         nav: false,
         arrows: false
     });
+    
+    
+    
+    //burguermenu
+    
 });
