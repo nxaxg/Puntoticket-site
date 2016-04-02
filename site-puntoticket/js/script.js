550b432a0405ee0d69f132b1dd9a5a294c95cc18
nxaxg <nxayancanxg@gmail.com>
2016-04-01 23:33:45 -0300

    Compress CSS.
    CSS compressed.

--- site-puntoticket/js/script.js
+++ site-puntoticket/js/script.js
@@ -1,26 +1,57 @@
 $(document).ready(function(){
-	//langmenu
-    $(".lang").hide();
-    $('#langbtn').click(function(){
-        $(this).toggleClass("lang-action");
-        $('.lang').slideToggle(500); 
+	//LANG-MENU
+    $(".lang-menu").hide();
+    $('#lang-btn').click(function(){
+        //hide burger-menu
+        $(".burguer-menu").slideUp(200);
+        //rotate up burguer-btn
+        $("#burguer-btn").removeClass("burguer-menu-action");
+        //rotate up categ-btn
+        $("#categ-icon").removeClass("categ-menu-action");
+        //rotate up lang-btn
+        $(this).toggleClass("lang-menu-action");
+        //show lang-menu
+        $('.lang-menu').slideToggle(500); 
     });
     
-    //burguer-menu action
-    $(".navmenu").hide();   
-    $('#menuicon').click(function(){
-        $('#menuicon').toggleClass("menu-action");
-        $(".navmenu").slideToggle(500);
+    //BURGUER-MENU
+    $(".burguer-menu").hide();   
+    $('#burguer-btn').click(function(){
+        //hide lang-menu
+        $(".lang-menu").slideUp(200);
+        $("#lang-btn").removeClass("lang-menu-action");
+        //hide categ-menu
+        $(".categ-menu").slideUp(200);
+        //rotate up categ-icon
+        $("#categ-icon").removeClass("categ-menu-action");
+        //rotate up burger-btn
+        $('#burguer-btn').toggleClass("burguer-menu-action");
+        //show burguer-menu
+        $(".burguer-menu").slideToggle(500);
 	});
     
-    //cat-menu
-    $(".cat-menu").hide();
-    $("#catbtn").click(function(){
-        $("#cat-arrow").toggleClass("cat-action");
-       $(".cat-menu").slideToggle(500); 
+    //CATEGORIES-MENU
+    $(".categ-menu").hide();
+    $("#categ-btn").click(function(){
+        //rotate up categ-icon
+        $("#categ-icon").toggleClass("categ-menu-action");
+        //show categ-icon
+       $(".categ-menu").slideToggle(500); 
     });
     
-    //slider action
+    //CERRAR-BTN
+    $("#cerrar-btn").click(function(){
+        //hide categ-menu
+       $(".categ-menu").slideUp(200);
+        //rotate up categ-btn
+        $("#categ-icon").removeClass("categ-menu-action");
+        //rotate up burguer-btn
+        $('#burguer-btn').removeClass("burguer-menu-action");
+        //hide burguer-menu
+        $(".burguer-menu").slideUp(500); 
+    });
+    
+    //SLIDER ACTION
     //http://unslider.com/
     $('#slider-main').unslider({
         autoplay: true,
@@ -29,9 +60,5 @@ $(document).ready(function(){
         nav: false,
         arrows: false
     });
-    
-    
-    
-    //burguermenu
-    
+        
 });
